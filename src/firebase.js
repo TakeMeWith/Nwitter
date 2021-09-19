fecb5abcb70c78a20f76f7164cbb73d157251d13
John Doe <swh45914255@gmail.com>
2021-09-19 12:43:10 +0900

    와 드디어 됐다...

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as impoty from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABFFbRSmwqgx6F4uopir-g3oaST7slqgE",
+    authDomain: "nwitter-d74fd.firebaseapp.com",
+    projectId: "nwitter-d74fd",
+    storageBucket: "nwitter-d74fd.appspot.com",
+    messagingSenderId: "1095861340924",
+    appId: "1:1095861340924:web:6c4838137d6eff8fdde579"
+  };    
+
+export default firebase.initializeApp(firebaseConfig);
